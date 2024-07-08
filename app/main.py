697f74058208bb53f10c6619e7d8d3f5057690f6
Alexander Oroudjev <a.oroudjev@gmail.com>
2024-07-08 00:50:47 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,45 +1,127 @@
 import sys
+from enum import Enum
 
 error_code = 0
 
 
-def tokenize(file_contents):
-    global error_code
-    line = 1
+class Scanner:
+    """
+    Scanner class scans the line and extracts tokens.
+    """
 
-    for c in file_contents:
-        match c:
-            case "(":
-                print("LEFT_PAREN ( null")
-            case ")":
-                print("RIGHT_PAREN ) null")
-            case "}":
-                print("RIGHT_BRACE } null")
-            case "{":
-                print("LEFT_BRACE { null")
-            case "*":
-                print("STAR * null")
-            case ".":
-                print("DOT . null")
-            case ",":
-                print("COMMA , null")
-            case "+":
-                print("PLUS + null")
-            case "-":
-                print("MINUS - null")
-            case ";":
-                print("SEMICOLON ; null")
-            case "\n":
-                line += 1
-            case "=":
-                print("EQUAL = null")
-            case "==":
-                print("EQUAL_EQUAL == null")
-            case _:
-                print(f'[line {line}] Error: Unexpected character: {c}', file=sys.stderr)
-                error_code = 65
+    def __init__(self, source):
+        self.error = False
+        self.tokens = []
+        self.source = source
 
-    print("EOF  null")
+    def advance(self, current):
+        return self.source[current + 1]
+
+    def scan(self):
+        # Scan the line and add found tokens to the tokens list
+        pointer = 0
+        line = 1
+        while pointer < len(self.source):
+            char = self.source[pointer]
+            match char:
+                case '\n':
+                    line += 1
+                case "(":
+                    self.tokens.append(Token(TokenType.LEFT_PAREN, self.get_token(pointer), "null", line))
+                case ")":
+                    self.tokens.append(Token(TokenType.RIGHT_PAREN, self.get_token(pointer), "null", line))
+                case "}":
+                    self.tokens.append(Token(TokenType.RIGHT_BRACE, self.get_token(pointer), "null", line))
+                case "{":
+                    self.tokens.append(Token(TokenType.LEFT_BRACE, self.get_token(pointer), "null", line))
+                case "*":
+                    self.tokens.append(Token(TokenType.STAR, self.get_token(pointer), "null", line))
+                case ".":
+                    self.tokens.append(Token(TokenType.DOT, self.get_token(pointer), "null", line))
+                case ",":
+                    self.tokens.append(Token(TokenType.COMMA, self.get_token(pointer), "null", line))
+                case "+":
+                    self.tokens.append(Token(TokenType.PLUS, self.get_token(pointer), "null", line))
+                case "-":
+                    self.tokens.append(Token(TokenType.RIGHT_PAREN, self.get_token(pointer), "null", line))
+                case ";":
+                    self.tokens.append(Token(TokenType.SEMICOLON, self.get_token(pointer), "null", line))
+                case "=":
+                    if self.advance(pointer) == '=':
+                        self.tokens.append(Token(TokenType.EQUAL_EQUAL, self.get_token_multi(pointer, 1), "null", line))
+                        pointer += 1
+                    else:
+                        self.tokens.append(Token(TokenType.EQUAL, self.get_token(pointer), "null", line))
+                case _:
+                    print(f'[line {line}] Error: Unexpected character: {char}', file=sys.stderr)
+                    self.error = True
+            pointer += 1
+
+    def get_token(self, index):
+        return self.get_token_multi(index, 0)
+
+    def get_token_multi(self, index_start, span):
+        return self.source[index_start: index_start + span]
+
+
+class Token:
+    """
+    Token class, contains all information of the token to return to the scanner.
+    """
+
+    def __init__(self, token_type, lexeme, literal, line):
+        self.type = token_type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self):
+        return f"{self.type.value} {self.lexeme} {self.literal}"
+
+
+class TokenType(Enum):
+    # Single-character tokens
+    LEFT_PAREN = "LEFT_PAREN"
+    RIGHT_PAREN = "RIGHT_PAREN"
+    LEFT_BRACE = "LEFT_BRACE"
+    RIGHT_BRACE = "RIGHT_BRACE"
+    COMMA = "COMMA"
+    DOT = "DOT"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    STAR = "STAR"
+    SEMICOLON = "SEMICOLON"
+    SLASH = "SLASH"
+
+    # One or two character tokens
+    BANG = "BANG"
+    BANG_EQUAL = "BANG_EQUAL"
+    EQUAL = "EQUAL"
+    EQUAL_EQUAL = "EQUAL_EQUAL"
+    GREATER = "GREATER"
+    GREATER_EQUAL = "GREATER_EQUAL"
+    LESS = "LESS"
+    LESS_EQUAL = "LESS_EQUAL"
+
+    # Keywords
+    AND = "AND"
+    OR = "OR"
+    CLASS = "CLASS"
+    ELSE = "ELSE"
+    WHILE = "WHILE"
+    VAR = "VAR"
+    FALSE = "FALSE"
+    TRUE = "TRUE"
+    THIS = "THIS"
+    RETURN = "RETURN"
+    PRINT = "PRINT"
+    IF = "IF"
+    NIL = "NIL"
+    FUN = "FUN"
+    SUPER = "SUPER"
+
+    # EOF
+    EOF = "EOF"
 
 
 def main():
@@ -57,8 +139,9 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-    tokenize(file_contents)
-    exit(error_code)
+    scanner = Scanner(file_contents)
+    scanner.scan()
+    print(*scanner.tokens, sep="\n")
 
 
 if __name__ == "__main__":
